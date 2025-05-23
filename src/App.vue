63afb30caa3489387dd2bac2bb1dd29bc3146658
eduardo-eiji-goto <eduardo@feraweb.com.br>
2025-05-23 14:12:49 -0300

    template commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+
+let video_id = ref("1087183197");
+let share_id = ref("af96c600c3");
+</script>
+
+<template>
+    <a
+        :href="`https://vimeo.com/${video_id}/${share_id}`"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        Share Link
+    </a>
+
+    <br />
+
+    <iframe
+        :src="`https://player.vimeo.com/video/${video_id}?h=${share_id}`"
+        frameborder="0"
+    />
+</template>
+
+<style scoped></style>
